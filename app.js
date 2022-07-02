6f0d4ed098893f1c018ad5a4b2029cda5a8fa2af
Manish Gupta <manish.info2020@gmail.com>
2022-07-02 15:35:04 +0530

    refector: app.js code

--- app.js
+++ app.js
@@ -1,54 +1,30 @@
-// console.log("Hello JavaScript")
-//  prompt("Enter your name?")
-//  alert("Welcome in JS world")
-
 var btnTranslate = document.querySelector("#btn")
 var textInput = document.querySelector("#textarea-box")
 var showOutput = document.querySelector("#output")
-console.log(btnTranslate)
-console.log(textInput)
-// var serverUrl = " https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=manish"
+
+// var serverUrl = " https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?"
 var serverUrl ="https://api.funtranslations.com/translate/minion.json"
 
-
-btn.addEventListener('click' , function clickEventHandler () {
-    console.log('Clicked')
-    console.log(textInput.value)
-
-    showOutput.innerText = "Hi this is your output" + textInput.value
-
-    var input = textInput.value
-
-
-
-fetch(getTranslationUrl(input))
-.then(response => response.json())
-.then(json => console.log(json.contents.translated))
-.catch(errorHandler)
-})
-
 function getTranslationUrl(text) {
-        return serverUrl + "?" + "text=" + text
+    return serverUrl + "?" + "text=" + text
 }
 
 function errorHandler(error) {
-    console.log("error occured" , error)
+console.log("error occured" , error)
 }
 
+btn.addEventListener('click' , clickEventHandler)
 
 
+function clickEventHandler () {
+var input = textInput.value
 
+fetch(getTranslationUrl(input))
+.then(response => response.json())
+.then(json => {
+    var translatedText = json.contents.translated
+    showOutput.innerText = translatedText}
+    )
 
-
-
-
-
-// textarea.addEventListener('onclick' , function () {
-//     console.log(textarea.value)
-//     console.log("hii")
-// })
-// fetch(serverUrl)
-// .then(response => response.json())
-// .then(json => console.log(json.contents.translated))
-// Promise {<pending>}
-// VM1027:3 Testing,  you are
+.catch(errorHandler)
+}
