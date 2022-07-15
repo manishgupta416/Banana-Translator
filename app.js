bfe960d6811506ba97f96895ffbad7665c018cfb
Manish Gupta <manish.info2020@gmail.com>
2022-07-15 11:43:43 +0530

    refactor and style

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var btnTranslate = document.querySelector("#btn")
 var textInput = document.querySelector("#textarea-box")
 var showOutput = document.querySelector("#output")
+var styleOutputBox = `
+background-color: #0533ab;
+font-size: large;
+font-weight: bold;
+color: white;`
 
-var serverUrl = " https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?"
+
+var serverUrl = " https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 // var serverUrl ="https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationUrl(text) {
@@ -22,10 +28,13 @@ console.log(inputTxt)
 
 fetch(getTranslationUrl(inputTxt))
 .then(response => response.json())
-.then(json =>{var translatedText = json.contents.translated
-    showOutput.innerText = translatedText}
+.then(json => 
+    // console.log(json)
+    {var translatedText = json.contents.translated
+    showOutput.innerText = translatedText
+    showOutput.style = styleOutputBox}
     )
     
-
 .catch(errorHandler)
 }
+
